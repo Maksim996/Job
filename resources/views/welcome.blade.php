2d2cd55b1d7848ca6bc650bf2e412da1d08b8959
Max <12usa34airbornE@gmail.com>
2019-05-02 12:19:19 +0300

    first back

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    JOB
                 </div>
 
                 <div class="links">
