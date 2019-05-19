72909794e8dd8d1fb48b988ccd8462b7e7a6e5c9
infernay <ani.batsman@gmail.com>
2019-05-19 16:33:02 +0300

    documents

--- app/Http/Controllers/Site/DocumentsController.php
+++ app/Http/Controllers/Site/DocumentsController.php
@@ -1,13 +1,31 @@
 <?php
 
 namespace App\Http\Controllers\Site;
+use DB;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class DocumentsController extends Controller
 {
     public function index(){
+        $documents = DB::table('documents')->get()->toArray();
 
-    	return view('site/documents');
+        $queryIdDocuments = DB::table('documents')
+            ->get(['subcategory_id'])
+            ->toArray();
+
+        $ids = [];
+        foreach ($queryIdDocuments as $item) {
+            array_push($ids, $item->subcategory_id);
+        }
+
+        $subCategories = DB::table('subcategory')->whereIn('subcategory_id',$ids)->get()->toArray();
+
+        $data=[
+            'documents' => $documents,
+            'subcategories' => $subCategories,
+        ];
+
+    	return view('site/documents',compact('data'));
     }
 }
--- resources/views/site/documents.blade.php
+++ resources/views/site/documents.blade.php
@@ -10,43 +10,38 @@
                 <div class="row justify-content-center">
                     <div class="col-md-12 col-lg-6 justify-content-center docum__btn">
                         <div class="nav flex-column nav-pills py-5" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                            <a class="nav-link btn-dark active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
-                            <a class="nav-link btn-dark " id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
-                            <a class="nav-link btn-dark " id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
-                            <a class="nav-link btn-dark " id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+                            @php($i = 0)
+                            @foreach($data['subcategories'] as $category)
+                                @if($i == 0)
+                                    <a class="nav-link btn-dark active" id="v-pills-{{$category->title}}-tab" data-toggle="pill" href="#v-pills-{{$category->title}}" role="tab" aria-controls="v-pills-{{$category->title}}" aria-selected="true">{{$category->title}}</a>
+                                   @else
+                                    <a class="nav-link btn-dark" id="v-pills-{{$category->title}}-tab" data-toggle="pill" href="#v-pills-{{$category->title}}" role="tab" aria-controls="v-pills-{{$category->title}}" aria-selected="true">{{$category->title}}</a>
+                                @endif
+                                @php($i++)
+                            @endforeach
                         </div>
                     </div>
                     <div class="col-md-12 col-lg-6 docum__doc py-3">
                         <div class="tab-content" id="v-pills-tabContent">
-                            <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
-                                <ul class="docum__ul">
-                                    <li class="docum__li"><a href="#" class="docum__link">Документ для розробки програмного забезпечення та покарання дизайнерів цих програмних забезпечень(v.1.3.7)</a></li>
-                                    <li class="docum__li"><a href="#" class="docum__link">Фото</a></li>
-                                    <li class="docum__li"><a href="#" class="docum__link">Печать</a></li>
-                                    <li class="docum__li"><a href="#" class="docum__link">Левий за 100$</a></li>
-                                    <li class="docum__li"><a href="#" class="docum__link">Фото</a></li>
-                                    <li class="docum__li"><a href="#" class="docum__link">Печать</a></li>
-                                    <li class="docum__li"><a href="#" class="docum__link">Левий за 100$</a></li>
-                                    <li class="docum__li"><a href="#" class="docum__link">Фото</a></li>
-                                    <li class="docum__li"><a href="#" class="docum__link">Печать</a></li>
-                                    <li class="docum__li"><a href="#" class="docum__link">Левий за 100$</a></li>
-                                    <li class="docum__li"><a href="#" class="docum__link">Печать</a></li>
-                                    <li class="docum__li"><a href="#" class="docum__link">Левий за 100$</a></li>
-                                    <li class="docum__li"><a href="#" class="docum__link">Фото</a></li>
-                                    <li class="docum__li"><a href="#" class="docum__link">Печать</a></li>
-                                </ul>
-                            </div>
-                            <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
-                                <ul class="docum__ul">
-                                    <li class="docum__li"><a href="#" class="docum__link">Документ для розробки програмного забезпечення та покарання дизайнерів цих програмних забезпечень(v.1.3.7)</a></li>
-                                    <li class="docum__li"><a href="#" class="docum__link">Фото</a></li>
-                                    <li class="docum__li"><a href="#" class="docum__link">Печать</a></li>
-                                    <li class="docum__li"><a href="#" class="docum__link">Левий за 100$</a></li>
-                                </ul>
-                            </div>
-                            <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci deleniti sapiente ea amet quam odit culpa doloremque ab, voluptatibus modi cupiditate laborum odio! Molestiae, recusandae, voluptatem laudantium ipsam earum ab.</div>
-                            <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci deleniti sapiente ea amet quam odit culpa doloremque ab, voluptatibus modi cupiditate laborum odio! Molestiae, recusandae, voluptatem laudantium ipsam earum ab.</div>
-                        </div>
+                            @php($i = 0)
+                            @foreach($data['subcategories'] as $category)
+                                @if($i == 0)
+                                    <div class="tab-pane fade show active" id="v-pills-{{$category->title}}" role="tabpanel" aria-labelledby="v-pills-{{$category->title}}-tab">
+                                @else
+                                    <div class="tab-pane fade show" id="v-pills-{{$category->title}}" role="tabpanel" aria-labelledby="v-pills-{{$category->title}}-tab">
+                                @endif
+                                        <ul class="docum__ul">
+                                            @foreach($data['documents'] as $document)
+                                                @if($document->subcategory_id == $category->subcategory_id)
+                                                <li class="docum__li"><a href="#" class="docum__link">{{$document->title}}</a></li>
+                                                @endif
+                                            @endforeach
+                                        </ul>
+                                @php($i++)
+                                     </div>
+                            @endforeach
+
+                           </div>
                     </div>
                 </div>
             </div>
