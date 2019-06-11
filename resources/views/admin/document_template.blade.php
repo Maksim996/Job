97d033133e71461825caf5aeaa3e103206298af7
infernay <ani.batsman@gmail.com>
2019-06-11 11:53:58 +0300

    documents save/delete/add NO FILE UPLOAD

--- resources/views/admin/document_template.blade.php
+++ resources/views/admin/document_template.blade.php
@@ -13,7 +13,7 @@
             </div>
         </div>
         
-        <form method="POST" action="{{ URL::route('ad_documents.documents.update', $data['document']->doc_id) }}"  class="k-form k-form--label-right" id='partners_blocks'>
+        <form method="POST" @if($data['type'] == '1') action="{{ URL::route('ad_documents.documents.update', $data['document']->doc_id) }}" @else action="{{ URL::route('ad_documents.documents.update',0) }}" @endif class="k-form k-form--label-right" id='partners_blocks'>
               {{ @csrf_field() }}
                @method('PUT') 
             <div class="k-portlet__body" id='documents_block'>
@@ -22,21 +22,27 @@
                         <label class="col-form-label col-lg-2 col-sm-12">Категорія документу:</label>
                         <select class="" required name="cat">
                         @foreach($data['subcategories'] as $sub)
-                            <option @if($data['document']->subcategory_id == $sub->subcategory_id) selected @endif value="{{$sub->subcategory_id}}">{{$sub->title}}</option>
+                            <option @if($data['type'] == '1')
+                                        @if($data['document']->subcategory_id == $sub->subcategory_id)
+                                            selected
+                                        @endif
+                                    @endif
+                                        value="{{$sub->subcategory_id}}">{{$sub->title}}
+                            </option>
                         @endforeach    
                         </select> 
                     </div>
                     <div class="form-group row">
                         <label class="col-form-label col-lg-2 col-sm-12">Заголовок документу</label>
                         <div class="col-lg-6 col-md-9 col-sm-12">
-                            <input type="text" class="form-control" placeholder="" name="title" value="{{$data['document']->title}}">
+                            <input type="text" class="form-control" placeholder="" name="title" @if($data['type'] == '1') value="{{$data['document']->title}}" @endif>
                         </div>
                     </div>
                     
                     <div class="form-group row">
                         <label class="col-form-label col-lg-2 col-sm-12">Посилання</label>
                         <div class="col-lg-6 col-md-9 col-sm-12">
-                            <input type="text" class="form-control" name="link" placeholder="" value="{{$data['document']->file_link}}">
+                            <input type="text" class="form-control" name="link" placeholder="" @if($data['type'] == '1') value="{{$data['document']->file_link}}" @endif>
                             <span class="form-text text-muted">По кліку переходить на посиланням ...</span> 
                         </div>
                     </div>
